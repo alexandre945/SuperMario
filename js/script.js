a5817490079ef5b057210178d20d59d2c5d67da2
alexandre <alexandresousaroberto@gmail.com>
2022-06-04 23:37:56 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+
+document.addEventListener('keydown', (e) => {
+    const mario = document.getElementById('mario');
+    mario.classList.add('jump');
+    setTimeout(() => {
+        const mario = document.getElementById('mario');
+    mario.classList.remove('jump'); 
+    }, 500)
+} );
+
+const loop = setInterval(() => {
+    const mario = document.getElementById('mario');
+    const pipe = document.getElementById('pipe');
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = + window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
+       pipe.style.animation = 'none'; 
+       pipe.style.left = `${pipePosition}px`;
+
+       mario.style.animation = 'none'; 
+       mario.style.bottom = `${marioPosition}px`;
+
+       mario.src = '../image/over.jpeg';
+       mario.style.width = '100px';
+       mario.style.marginLeft = '50px';
+
+       clearInterval(loop);
+    }
+  
+
+}, 10);
+
